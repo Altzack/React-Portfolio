14d7602250a77e7fc070ec9154f74b069858f675
Zack <altschulerzack@yahoo.com>
2020-11-09 00:31:23 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components/macro';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
+import FourOhFour from './containers/common/FourOhFour';
+import { useResponsive } from './containers/common/responsiveComponents';
+import Footer from './containers/common/Footer';
+import Header from './containers/common/Header';
+import AppContext from './AppContext';
+import LandingPage from './containers/LandingPage/LandingPage';
 
-function App() {
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  color: rgba(232, 230, 227, 0.85);
+  background-color: rgba(250, 250, 250);
+  ${({ isMobile }) => isMobile && 'overflow-x: hidden;'}
+  max-width: 100%;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  padding-top: 64px;
+  min-height: 100vh;
+`;
+
+const AppWrapper = withRouter(({ children }) => {
+  const { isTabletOrMobile } = useResponsive();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer isMobile={isTabletOrMobile}>
+      <Header />
+      <ContentContainer>{children}</ContentContainer>
+      <Footer />
+    </AppContainer>
   );
+});
+
+class App extends Component {
+  render() {
+    const contextValues = {};
+    return (
+      <AppContext.Provider value={contextValues}>
+        <>
+          <Router>
+            <QueryParamProvider ReactRouterRoute={Route}>
+              <AppWrapper>
+                <Switch>
+                  <Route exact path="/">
+                    <LandingPage />
+                  </Route>
+                  <Route>
+                    <FourOhFour />
+                  </Route>
+                </Switch>
+              </AppWrapper>
+            </QueryParamProvider>
+          </Router>
+        </>
+      </AppContext.Provider>
+    );
+  }
 }
 
 export default App;
