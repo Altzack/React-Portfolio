18d0b2021dcd5a64d783aec3f02d3b7d847cd8a0
zack altschuler <altschulerzack@yahoo.com>
2023-03-14 13:46:47 -0700

    fix

--- src/Projects.js
+++ src/Projects.js
@@ -15,32 +15,13 @@ const projects = [
     repoLink: 'https://github.com/Altzack/CryptoCurrencyTracker',
     appLink: 'https://cryptotrack-eight.vercel.app/',
   },
-  // {
-  //   title: 'Covid Tracker',
-  //   desc: 'A covid dashboard with daily updated data.',
-  //   img: 'images/covid.png',
-  //   langs: 'React, CSS',
-  //   repoLink: 'https://github.com/Altzack/Covid-Tracker',
-  //   appLink: 'https://covidtrack1.netlify.app/',
-  // },
-
   {
-    title: 'Boozy',
-    desc:
-      'A platform to Browse, create and share custom mixed drink recipes with your friends, family, etc. This was built as my first full-stack capstone for Bloc.',
-    img: 'images/boozy.png',
-    langs: 'PostgreSQL, Express, React, Node',
-    repoLink: 'https://github.com/Altzack/Boozy',
-    appLink: 'https://boozy.vercel.app/',
-  },
-  {
-    title: 'Stock-Dash',
-    desc:
-      'A dashboard that allows you to get the latest news and pricing for any NASDAQ traded symbol.',
-    img: 'images/stockdash.png',
-    langs: 'PostgreSQL, Express, React, Node',
-    repoLink: 'https://github.com/Altzack/Stock-Dash',
-    appLink: 'https://stock-dash.vercel.app/',
+    title: 'Covid Tracker',
+    desc: 'A covid dashboard with daily updated data.',
+    img: 'images/covid.png',
+    langs: 'React, CSS',
+    repoLink: 'https://github.com/Altzack/Covid-Tracker',
+    appLink: 'https://covidtrack1.netlify.app/',
   },
 ];
 export default projects;
